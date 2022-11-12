953d008d8325ea4cbe8fb01923e417f7de2f5201
Luiz Felipe Warmling <luizfelipewarmling@gmail.com>
2022-11-12 13:47:31 -0300

    [FEAT] Alter refs

--- src/npm/markdown.rs
+++ src/npm/markdown.rs
@@ -2,23 +2,23 @@
 
 // Use NPM or Rust code in util.rs if necessary
 
-// import { lexer, parser } from "marked";
-// import showdown from "showdown";
+import { lexer, parser } from "marked";
+import showdown from "showdown";
 
-// import { memoizeWith, identity } from "./ramda";
+import { memoizeWith, identity } from "./ramda";
 
-// // memoizeWith only for pure functio
-// let html = (input = "") => {
-//   const test = parser(lexer(input));
-//   return test;
-// };
+// memoizeWith only for pure function
+let html = (input = "") => {
+  const test = parser(lexer(input));
+  return test;
+};
 
-// html = memoizeWith(identity, html);
+html = memoizeWith(identity, html);
 
-// let markdown = (input = "") => {
-//     const converter = new showdown.Converter();
-//     const result = converter.makeMarkdown(input);
-//     return result;
-// }
+let markdown = (input = "") => {
+    const converter = new showdown.Converter();
+    const result = converter.makeMarkdown(input);
+    return result;
+}
 
-// markdown = memoizeWith(identity, markdown);
+markdown = memoizeWith(identity, markdown);
