7f541ab26b7377baed1207d89fbff0bc62cd6bd4
Luiz Felipe Warmling <luizfelipewarmling@gmail.com>
2022-11-12 13:46:24 -0300

    [FEAT] MD memoize

---
+++ src/npm/markdown.rs
@@ -0,0 +1,24 @@
+// https://github.com/DenisKolodin/yew/blob/master/examples/inner_html/src/lib.rs with marked
+
+// Use NPM or Rust code in util.rs if necessary
+
+// import { lexer, parser } from "marked";
+// import showdown from "showdown";
+
+// import { memoizeWith, identity } from "./ramda";
+
+// // memoizeWith only for pure functio
+// let html = (input = "") => {
+//   const test = parser(lexer(input));
+//   return test;
+// };
+
+// html = memoizeWith(identity, html);
+
+// let markdown = (input = "") => {
+//     const converter = new showdown.Converter();
+//     const result = converter.makeMarkdown(input);
+//     return result;
+// }
+
+// markdown = memoizeWith(identity, markdown);
